0d11b57afdb4efd8e6505da52d33dbac6ef826ba
Denis <denis16_08@mail.ru>
2024-09-17 21:20:15 +0300

    Добавлен метод загруженности работы, внесены изменения в main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from colors import color_coefficients
 from parts import part_coefficients
 from paintMethods import paintMethod_coefficients
 from calculator import calculate_cost
+from workload import workload_coefficients
 
 # Базовая стоимость
 base_price = 12000
@@ -10,10 +11,11 @@ base_price = 12000
 part = input("Введите наименование детали: ").lower()
 color = input("Введите цвет: ").lower()
 paintMethod = input("Введите метод покраски: ").lower()
+workload = input("Введите загруженность сервиса: ").lower()
 
 try:
     # Рассчитываем стоимость
-    total_cost = calculate_cost(base_price, part, color, paintMethod, part_coefficients, color_coefficients, paintMethod_coefficients)
+    total_cost = calculate_cost(base_price, part, color, paintMethod, part_coefficients, color_coefficients, paintMethod_coefficients, workload_coefficients)
     print(f"Стоимость покраски: {total_cost} рублей")
 except ValueError as e:
     print(e)
