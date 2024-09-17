7dc16a57bdb2c73265c0b9a4efb5041bd3100aa6
Dimlenn <d_melen@mail.ru>
2024-09-17 20:53:32 +0300

    Добавлен выбор метода покраски детали, внесены изменения в классы main и zzczcalculator

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-def calculate_cost(base_price, part, color, part_coefficients, color_coefficients):
+def calculate_cost(base_price, part, color, paintMethod, part_coefficients, color_coefficients, paintMethod_coefficients):
     if part in part_coefficients and color in color_coefficients:
-        total_cost = base_price * part_coefficients[part] * color_coefficients[color]
+        total_cost = base_price * part_coefficients[part] * color_coefficients[color] * paintMethod_coefficients[paintMethod]
         return total_cost
     else:
         raise ValueError("Incorrect color or part")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from colors import color_coefficients
 from parts import part_coefficients
+from paintMethods import paintMethod_coefficients
 from calculator import calculate_cost
 
 # Базовая стоимость
@@ -8,10 +9,11 @@ base_price = 12000
 # Ввод данных
 part = input("Введите наименование детали: ").lower()
 color = input("Введите цвет: ").lower()
+paintMethod = input("Введите метод покраски: ").lower()
 
 try:
     # Рассчитываем стоимость
-    total_cost = calculate_cost(base_price, part, color, part_coefficients, color_coefficients)
+    total_cost = calculate_cost(base_price, part, color, paintMethod, part_coefficients, color_coefficients, paintMethod_coefficients)
     print(f"Стоимость покраски: {total_cost} рублей")
 except ValueError as e:
     print(e)
